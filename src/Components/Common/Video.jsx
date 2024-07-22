767157748ecd501cf9163818e520c7623228d5a9
lawliet005 <sabalomsadze912@gmail.com>
2024-07-22 21:33:50 +0400

    video fixing comitt 1

--- src/Components/Common/Video.jsx
+++ src/Components/Common/Video.jsx
@@ -2,6 +2,15 @@ import video from "media/mainVideo.mp4";
 
 export const MainVideo = () => {
   return (
-    <video className="main-video" loop muted autoPlay playsInline src={video} />
+    <video
+      className="main-video"
+      loop
+      muted
+      autoPlay
+      playsInline
+      preload="auto"
+      controlsList="nodownload"
+      src={video}
+    />
   );
 };
