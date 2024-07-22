5614e12b77a95925ceb67b747fb9941145e9e1a3
lomsa005 <sabalomsadze912@gmail.com>
2024-07-22 15:38:30 +0400

    update footer lines

--- src/Components/Common/Video.jsx
+++ src/Components/Common/Video.jsx
@@ -4,8 +4,6 @@ export const MainVideo = () => {
   return (
     <video autoPlay muted loop className="main-video">
       <source src={video} type="video/mp4" />
-      <source src={video} type="video/ogg" />
-      <source src={video} type="video/webm" />
       Your browser does not support the video tag.
     </video>
   );
