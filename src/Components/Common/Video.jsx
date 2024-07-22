0d4d5e1ff161dc0c68a00923f49c6dc272f55a2b
vako giginashvili <vgiginashvili@seu.edu.ge>
2024-07-22 11:01:28 +0400

    possible solution of video displaying

--- src/Components/Common/Video.jsx
+++ src/Components/Common/Video.jsx
@@ -3,7 +3,9 @@ import video from "media/mainVideo.mp4";
 export const MainVideo = () => {
   return (
     <video autoPlay muted loop className="main-video">
-      <source src={video} type="video/mp4"/>
+      <source src={video} type="video/mp4" />
+      <source src={video} type="video/ogg" />
+      <source src={video} type="video/webm" />
     </video>
   );
 };
