1cc18520914678c3bfd8191bc7939418cc2dda09
Levan Jokhadze <you@example.co>
2024-07-23 17:46:55 +0400

    boxes update

--- src/Components/Common/boxes.jsx
+++ src/Components/Common/boxes.jsx
@@ -6,13 +6,23 @@ import fourthBox from "media/fourthBox.png";
 
 export const Boxes = () => {
   return (
-    <div className="boxContainer">
-      <img className="box box1" src={firstBox}>
-        {/* <p className="boxParagraph"></p> */}
-      </img>
-      <img className="box box2" src={secondBox}></img>
-      <img className="box box3" src={thirdBox}></img>
-      <img className="box box4" src={fourthBox}></img>
+    <div className="boxes">
+      <div className="boxContainer boxContainer1">
+        <img className="box box1" src={firstBox}></img>
+        <p className="boxParagraph boxParagraph1">პროდუქტები</p>
+      </div>
+      <div className="boxContainer boxContainer2">
+        <img className="box box2" src={secondBox}></img>
+        <p className="boxParagraph boxParagraph2">სერვისები</p>
+      </div>
+      <div className="boxContainer boxContainer3">
+        <img className="box box3" src={thirdBox}></img>
+        <p className="boxParagraph boxParagraph3">პორტფოლიო</p>
+      </div>
+      <div className="boxContainer boxContainer4">
+        <img className="box box4" src={fourthBox}></img>
+        <p className="boxParagraph boxParagraph4">ჩვენ შესახებ</p>
+      </div>   
     </div>
   );
 };
