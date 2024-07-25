22c2fb56c996659a4dac0032432f3b8230c89009
vako giginashvili <vgiginashvili@seu.edu.ge>
2024-07-25 19:30:27 +0400

    fixed video displaying issue

--- src/Components/Common/Video.jsx
+++ src/Components/Common/Video.jsx
@@ -1,16 +1,47 @@
+import { useEffect, useRef } from 'react'
 import video from "media/mainVideo.mp4";
 
 export const MainVideo = () => {
+  const videoRef = useRef(null);
+  
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const attemptPlay = () => {
+      videoElement.play().catch(error => {
+        console.error('Error attempting to play the video:', error)
+      })
+    }
+
+    if (videoElement) {
+      
+      videoElement.load()
+
+      attemptPlay()
+
+      const intervalId = setInterval(() => {
+        if (videoElement.paused) {
+          attemptPlay()
+        } else {
+          clearInterval(intervalId)
+        }
+      }, 1000)
+
+      return () => clearInterval(intervalId)
+    }
+  }, [])
+  
   return (
     <video
-      className="main-video"
-      loop
-      muted
-      autoPlay
-      playsInline
-      preload="auto"
-      controlsList="nodownload"
-      src={video}
+    ref={videoRef}
+    src={video}
+    autoPlay
+    muted
+    loop
+    playsInline
+    // controlsList="nodownload"
+    className="main-video"
+    preload="auto"
     />
   );
 };
