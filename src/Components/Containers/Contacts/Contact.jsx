8569907cb2edb262f3bd102e30b126eab35ad15d
NugoChubinidze <nugonugonugo10@gmail.com>
2024-07-30 04:16:59 +0400

    responsive

--- src/Components/Containers/Contacts/Contact.jsx
+++ src/Components/Containers/Contacts/Contact.jsx
@@ -4,16 +4,21 @@ import email from "media/email.svg";
 import location from "media/location.svg";
 import number from "media/number.svg";
 import "./Contact.scss";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 export const Contact = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
 
   return (
     <>
-    <div className="ContactBorder">
-      <ContactBorder />
-      <img src={btnclose} alt="close button" className="CloseBtn" onClick={onClose} />
+      <div className="ContactBorder">
+        <ContactBorder />
+        <img
+          src={btnclose}
+          alt="close button"
+          className="CloseBtn"
+          onClick={onClose}
+        />
 
         <div className="ContactElements">
           <div className="scrollable-content">
@@ -35,7 +40,6 @@ export const Contact = ({ isVisible, onClose }) => {
                 </div>
                 <div className="ws">დაგვიკავშირდით</div>
               </div>
-              
               <div className="smCont">
                 <img src={email} />
                 <div className="fl">
@@ -63,43 +67,49 @@ export const Contact = ({ isVisible, onClose }) => {
               <div className="fill">
                 <div className="label">
                   <label htmlFor="FirstName">სახელი</label>
-                  <p>LUKA</p>
+                  <input type="text" id="FirstName" name="FirstName" />
                 </div>
                 <div className="filled"></div>
               </div>
               <div className="fill">
                 <div className="label">
                   <label htmlFor="LastName">გვარი</label>
+                  <input type="text" id="LastName" name="LastName" />
                 </div>
                 <div className="notfill"></div>
               </div>
               <div className="fill">
                 <div className="label">
                   <label htmlFor="Email">ელ:ფოსტა</label>
+                  <input type="email" id="Email" name="Email" />
                 </div>
                 <div className="notfill"></div>
               </div>
               <div className="fill">
                 <div className="label">
                   <label htmlFor="Phone">ტელეფონის ნომერი</label>
+                  <input type="tel" id="Phone" name="Phone" />
                 </div>
                 <div className="notfill"></div>
               </div>
               <div className="fill">
                 <div className="label">
                   <label htmlFor="serviceName">სერვისის დასახელება</label>
+                  <input type="text" id="serviceName" name="serviceName" />
                 </div>
                 <div className="notfill"></div>
               </div>
               <div className="fill">
                 <div className="label">
                   <label htmlFor="CompanyName">კომპანიის სახელი</label>
+                  <input type="text" id="CompanyName" name="CompanyName" />
                 </div>
                 <div className="notfill"></div>
               </div>
               <div className="longLine fill">
                 <div className="label">
                   <label htmlFor="Comment">დაწერეთ კომენტარი</label>
+                  <textarea id="Comment" name="Comment"></textarea>
                 </div>
                 <div className="notfill"></div>
               </div>
