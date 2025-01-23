15d8e133ac964907f03149834034b9ab528a690e
lawliet005 <sabalomsadze912@gmail.com>
2025-01-23 22:35:48 +0400

    Add NotFound component with Lottie animation and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { HomePage, Layout } from './Components';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { DataProvider } from './Components/Api/Api';
 import { LanguageProvider } from './Components/Common/LanguageContext';
+import NotFound from './Components/Common/NotFound';
 
 function App() {
 
   return (
     <>
       <Router>
-      <Routes>
-        <Route path="/" element={<LanguageProvider><DataProvider><Layout> <HomePage/></Layout></DataProvider></LanguageProvider>} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<LanguageProvider><DataProvider><Layout> <HomePage/></Layout></DataProvider></LanguageProvider>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
   )
 }
-
 
 export default App;
---
+++ src/Components/Common/NotFound.jsx
@@ -0,0 +1,13 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const NotFound = () => {
+  return (
+    <DotLottieReact
+      src="/animation.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+
+export default NotFound;
