36eee3f61bc384b70d61e2100d722c46e01e77f9
lawliet005 <sabalomsadze912@gmail.com>
2025-01-23 20:54:21 +0400

    Refactor Contact component for improved input handling

--- src/Components/Containers/Contacts/Contact.jsx
+++ src/Components/Containers/Contacts/Contact.jsx
@@ -13,15 +13,16 @@ import { useLanguage } from '../../Common/LanguageContext';
 export const Contact = ({ isVisible, onClose }) => {
   const { contactData } = useData();
   const { isGeo } = useLanguage();
-  const [inputStates, setInputStates] = useState({
-    FirstName: false,
-    LastName: false,
-    Email: false,
-    Phone: false,
-    serviceName: false,
-    CompanyName: false,
-    Comment: false
+  const [formData, setFormData] = useState({
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Phone: '',
+    serviceName: '',
+    CompanyName: '',
+    Comment: ''
   });
+
   const [isFocused, setIsFocused] = useState(isVisible);
   const contactBorderRef = useRef(null);
   const [progress, setProgress] = useState(0);
@@ -30,43 +31,42 @@ export const Contact = ({ isVisible, onClose }) => {
   useEffect(() => {
     const savedFormData = JSON.parse(localStorage.getItem('formData'));
     if (savedFormData) {
-      setInputStates(savedFormData);
+      setFormData(savedFormData);
     }
   }, []);
-
-  const calculateProgress = () => {
-    let filledCount = 0;
-    for (const key in inputStates) {
-      if (inputStates[key]) {
-        filledCount++;
-      }
-    }
-    return (filledCount / Object.keys(inputStates).length) * 100;
-  };
 
   useEffect(() => {
     const contactBorder = contactBorderRef.current;
     if (isVisible) {
       setIsFocused(isVisible);
       setTimeout(() => {
-        contactBorder.classList.add('visible');
-      }, 1);
+        contactBorder?.classList?.add('visible');
+      }, 10);
     } else {
       if(contactBorder){
         setTimeout(() => {
           contactBorder.classList.remove('visible');
-        }, 1);
+        }, 10);
       }
       setTimeout(() => {
         setIsFocused(isVisible);
       }, 500)
     }
-    
   }, [isFocused, isVisible]);
+
+  const calculateProgress = () => {
+    let filledCount = 0;
+    for (const key in formData) {
+      if (formData[key] !== '') {
+        filledCount++;
+      }
+    }
+    return (filledCount / Object.keys(formData).length) * 100;
+  };
 
   useEffect(() => {
     setProgress(calculateProgress());
-  }, [inputStates]);
+  }, [formData]);
 
   const inputRefs = {
     FirstName: useRef(null),
@@ -95,7 +95,7 @@ export const Contact = ({ isVisible, onClose }) => {
   };
 
   const handleInputBlur = (inputId) => {
-    if (!inputStates[inputId] && labelRefs[inputId].current) {
+    if (formData[inputId] === '' && labelRefs[inputId].current) {
       labelRefs[inputId].current.classList.remove('focused');
     }
   };
@@ -111,35 +111,21 @@ export const Contact = ({ isVisible, onClose }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const formData = {
-      name: inputRefs.FirstName.current.value,
-      last_name: inputRefs.LastName.current.value,
-      email: inputRefs.Email.current.value,
-      number: inputRefs.Phone.current.value,
-      service: inputRefs.serviceName.current.value,
-      company: inputRefs.CompanyName.current.value,
-      message: inputRefs.Comment.current.value
-    };
-
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_ENDPOINT}/contacts`, formData);
       console.log('Response from server:', response.data);
 
       // Clear form data from localStorage after successful submission
       localStorage.removeItem('formData');
-      setInputStates({
-        FirstName: false,
-        LastName: false,
-        Email: false,
-        Phone: false,
-        serviceName: false,
-        CompanyName: false,
-        Comment: false
+      setFormData({
+        FirstName: '',
+        LastName: '',
+        Email: '',
+        Phone: '',
+        serviceName: '',
+        CompanyName: '',
+        Comment: ''
       });
-      // Resetting the form fields to empty
-      for (const key in inputRefs) {
-        inputRefs[key].current.value = '';
-      }
 
       // Resetting labels
       for (const key in labelRefs) {
@@ -162,12 +148,12 @@ export const Contact = ({ isVisible, onClose }) => {
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
-    const newStates = {
-      ...inputStates,
+    const newFormData = {
+      ...formData,
       [id]: value
     };
-    setInputStates(newStates);
-    localStorage.setItem('formData', JSON.stringify(newStates));
+    setFormData(newFormData);
+    localStorage.setItem('formData', JSON.stringify(newFormData));
   };
 
   return (
@@ -226,7 +212,7 @@ export const Contact = ({ isVisible, onClose }) => {
               <div className="FillContact">
                 <div className="fill" onClick={() => handleLineClick('FirstName')}>
                   <div className="label">
-                    <label ref={labelRefs.FirstName} htmlFor="FirstName">{isGeo ? 'სახელი' : 'First Name'}</label>
+                    <label ref={labelRefs.FirstName} htmlFor="FirstName" className={formData.FirstName !== '' ? 'focused' : ''}>{isGeo ? 'სახელი' : 'First Name'}</label>
                     <input
                       ref={inputRefs.FirstName}
                       type="text"
@@ -235,14 +221,14 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('FirstName')}
                       onBlur={() => handleInputBlur('FirstName')}
-                      value={inputRefs.FirstName.current?.value || JSON.parse(localStorage.getItem('formData'))?.FirstName || ''}
+                      value={formData.FirstName}
                     />
                   </div>
-                  <div className={inputStates.FirstName ? "filled" : "notfill"}></div>
+                  <div className={formData.FirstName !== '' ? "filled" : "notfill"}></div>
                 </div>
                 <div className="fill" onClick={() => handleLineClick('LastName')}>
                   <div className="label">
-                    <label ref={labelRefs.LastName} htmlFor="LastName">{isGeo ? 'გვარი' : 'Last Name'}</label>
+                    <label ref={labelRefs.LastName} htmlFor="LastName" className={formData.LastName !== '' ? 'focused' : ''}>{isGeo ? 'გვარი' : 'Last Name'}</label>
                     <input
                       ref={inputRefs.LastName}
                       type="text"
@@ -251,14 +237,14 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('LastName')}
                       onBlur={() => handleInputBlur('LastName')}
-                      value={inputRefs.LastName.current?.value || JSON.parse(localStorage.getItem('formData'))?.LastName || ''}
+                      value={formData.LastName}
                     />
                   </div>
-                  <div className={inputStates.LastName ? "filled" : "notfill"}></div>
+                  <div className={formData.LastName !== '' ? "filled" : "notfill"}></div>
                 </div>
                 <div className="fill" onClick={() => handleLineClick('Email')}>
                   <div className="label">
-                    <label ref={labelRefs.Email} htmlFor="Email">{isGeo ? 'ელ:ფოსტა' : 'Email'}</label>
+                    <label ref={labelRefs.Email} htmlFor="Email" className={formData.Email !== '' ? 'focused' : ''}>{isGeo ? 'ელ:ფოსტა' : 'Email'}</label>
                     <input
                       ref={inputRefs.Email}
                       type="email"
@@ -267,14 +253,14 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('Email')}
                       onBlur={() => handleInputBlur('Email')}
-                      value={inputRefs.Email.current?.value || JSON.parse(localStorage.getItem('formData'))?.Email || ''}
+                      value={formData.Email}
                     />
                   </div>
-                  <div className={inputStates.Email ? "filled" : "notfill"}></div>
+                  <div className={formData.Email !== '' ? "filled" : "notfill"}></div>
                 </div>
                 <div className="fill" onClick={() => handleLineClick('Phone')}>
                   <div className="label">
-                    <label ref={labelRefs.Phone} htmlFor="Phone">{isGeo ? 'ტელეფონის ნომერი' : 'Phone Number'}</label>
+                    <label ref={labelRefs.Phone} htmlFor="Phone" className={formData.Phone !== '' ? 'focused' : ''}>{isGeo ? 'ტელეფონის ნომერი' : 'Phone Number'}</label>
                     <input
                       ref={inputRefs.Phone}
                       type="tel"
@@ -283,14 +269,14 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('Phone')}
                       onBlur={() => handleInputBlur('Phone')}
-                      value={inputRefs.Phone.current?.value || JSON.parse(localStorage.getItem('formData'))?.Phone || ''}
+                      value={formData.Phone}
                     />
                   </div>
-                  <div className={inputStates.Phone ? "filled" : "notfill"}></div>
+                  <div className={formData.Phone !== '' ? "filled" : "notfill"}></div>
                 </div>
                 <div className="fill" onClick={() => handleLineClick('serviceName')}>
                   <div className="label">
-                    <label ref={labelRefs.serviceName} htmlFor="serviceName">{isGeo ? 'სერვისის დასახელება' : 'Service Name'}</label>
+                    <label ref={labelRefs.serviceName} htmlFor="serviceName" className={formData.serviceName !== '' ? 'focused' : ''}>{isGeo ? 'სერვისის დასახელება' : 'Service Name'}</label>
                     <input
                       ref={inputRefs.serviceName}
                       type="text"
@@ -299,14 +285,14 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('serviceName')}
                       onBlur={() => handleInputBlur('serviceName')}
-                      value={inputRefs.serviceName.current?.value || JSON.parse(localStorage.getItem('formData'))?.serviceName || ''}
+                      value={formData.serviceName}
                     />
                   </div>
-                  <div className={inputStates.serviceName ? "filled" : "notfill"}></div>
+                  <div className={formData.serviceName !== '' ? "filled" : "notfill"}></div>
                 </div>
                 <div className="fill" onClick={() => handleLineClick('CompanyName')}>
                   <div className="label">
-                    <label ref={labelRefs.CompanyName} htmlFor="CompanyName">{isGeo ? 'კომპანიის სახელი' : 'Company Name'}</label>
+                    <label ref={labelRefs.CompanyName} htmlFor="CompanyName" className={formData.CompanyName !== '' ? 'focused' : ''}>{isGeo ? 'კომპანიის სახელი' : 'Company Name'}</label>
                     <input
                       ref={inputRefs.CompanyName}
                       type="text"
@@ -315,14 +301,14 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('CompanyName')}
                       onBlur={() => handleInputBlur('CompanyName')}
-                      value={inputRefs.CompanyName.current?.value || JSON.parse(localStorage.getItem('formData'))?.CompanyName || ''}
+                      value={formData.CompanyName}
                     />
                   </div>
-                  <div className={inputStates.CompanyName ? "filled" : "notfill"}></div>
+                  <div className={formData.CompanyName !== '' ? "filled" : "notfill"}></div>
                 </div>
                 <div className="longLine fill" onClick={() => handleLineClick('Comment')}>
                   <div className="label cc">
-                    <label ref={labelRefs.Comment} htmlFor="Comment">{isGeo ? 'დაწერეთ კომენტარი' : 'Message'}</label>
+                    <label ref={labelRefs.Comment} htmlFor="Comment" className={formData.Comment !== '' ? 'focused' : ''}>{isGeo ? 'დაწერეთ კომენტარი' : 'Message'}</label>
                     <textarea
                       ref={inputRefs.Comment}
                       id="Comment"
@@ -330,10 +316,10 @@ export const Contact = ({ isVisible, onClose }) => {
                       onChange={handleInputChange}
                       onFocus={() => handleInputFocus('Comment')}
                       onBlur={() => handleInputBlur('Comment')}
-                      value={inputRefs.Comment.current?.value || JSON.parse(localStorage.getItem('formData'))?.Comment || ''}
+                      value={formData.Comment}
                     ></textarea>
                   </div>
-                  <div className={inputStates.Comment ? "filled" : "notfill"}></div>
+                  <div className={formData.Comment !== '' ? "filled" : "notfill"}></div>
                 </div>
               </div>
               <div className="ContactBtn" onClick={handleSubmit}>
